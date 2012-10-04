aae4d3116c27f59fc5aad0007e1edb30389b1455
Oliver Fesseler <oliver.fesseler@leomedia.eu>
2012-10-04 15:07:47 +0200

    Tests ausgelagert und Strings
    Jetzt am besten noch ne klasse drum rum bauen

---
+++ MantiacrawlStrings.py
@@ -0,0 +1,31 @@
+site_string = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	<head>
+		<title>Mantia  &raquo; Wallpaper</title>
+	</head>
+	<body class="wallpaper thursday oct4">
+		<div id="content">
+			<ul id="navigation">
+				<li class="home"><a href="http://mantia.me">Home</a></li>
+				<li class="wallpaper"><a href="http://mantia.me/wallpaper/">Wallpaper</a></li>
+			</ul>
+			<div id="feature">
+				<img src="http://mantia.me/images/tomorrowlandcocacola_large.jpg" alt="Tomorrowland Coca-Cola" />
+			</div>
+			<div id="featuremeta">
+				<div id="downloads">
+					<a class="iphone" href="http://mantia.me/goodies/desktops/tomorrowlandcocacola_iphone.jpg" title="Download iPhone Wallpaper">Download iPhone Wallpaper</a>
+					<a class="fullscreen" href="http://mantia.me/goodies/desktops/tomorrowlandcocacola_full.jpg" title="Download iPad Wallpaper">Download iPad Wallpaper</a>
+					<a class="widescreen" href="http://mantia.me/goodies/desktops/tomorrowlandcocacola_wide.jpg" title="Download Desktop Wallpaper">Download Desktop Wallpaper</a>
+				</div>
+				<h2>Tomorrowland Coca-Cola</h2>
+			</div>
+			<div id="archives">
+				<a href="http://mantia.me/wallpaper/tomorrowland-coke/" id="selected_thumbnail"><img src="http://mantia.me/images/tomorrowlandcocacola_small.jpg" alt="Tomorrowland Coca-Cola" title="Tomorrowland Coca-Cola" /></a>
+				<a href="http://mantia.me/wallpaper/red-bricks/"><img src="http://mantia.me/images/redbricks_small.jpg" alt="Red Bricks" title="Red Bricks" /></a>
+				<a href="http://mantia.me/wallpaper/orange-bricks/"><img src="http://mantia.me/images/orangebricks_small.jpg" alt="Orange Bricks" title="Orange Bricks" /></a>
+			<hr /></div>
+		</div>
+	</body>
+</html>"""
+
---
+++ MantiacrawlTest.py
@@ -0,0 +1,6 @@
+import MantiacrawlStrings
+import Mantiacrawl
+
+m = Mantiacrawl
+site = MantiacrawlStrings.site_string
+print m.getLinks(site)
